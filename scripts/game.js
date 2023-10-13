ef2eacbf0b896f1b6a5f097e2838f726306c9c79
renzalhades <renzalhades@gmail.com>
2023-10-13 12:47:33 +0800

    Display score and result through alert function.

--- scripts/game.js
+++ scripts/game.js
@@ -1,48 +1,86 @@
 function computerSelection ()
-{   
-    var computerSelect;
-    computerSelect = parseInt (Math.random ()*3);
-    switch (computerSelect)
-        {
-            case (0):
-                computerSelect = "paper";
-                break;
-            case (1):
-                computerSelect = "scissor";
-                break;
-            case (2):
-                computerSelect = "rock";
-                break;
-        }
-    return computerSelect;    
-}
+    {   
+        let computerChoice;
+        computerChoice = parseInt (Math.random ()*3);
+        switch (computerChoice)
+            {
+                case (0):
+                    computerChoice = "paper";
+                    break;
+                case (1):
+                    computerChoice = "scissor";
+                    break;
+                case (2):
+                    computerChoice = "rock";
+                    break;
+            }
+        return computerChoice;    
+    }
 
-function gameRound (playerSelect)
-{
-    playerSelect = playerSelect.toLowerCase();
-    let result, outcome, computerSelect = computerSelection ();
+function playerSelection ()
+    {   
+        let playerChoice = prompt ();
+        return (playerChoice.toLowerCase());
+    }
 
-    switch (true)
-        {   
-            case (playerSelect == computerSelect):
-                result = "Draw!";
-                break;
+function oneRound (playerSelect, computerSelect)
+    { 
+        let result, winner;
 
-            case ((playerSelect == "scissor" && computerSelect == "paper") || 
-                  (playerSelect == "rock" && computerSelect == "scissor") || 
-                  (playerSelect == "paper" && computerSelect == "rock") ):
-                result = "You Win! " + playerSelect + " beats " + computerSelect + "." ;
-                break;
+        switch (true)
+            {   
+                case (playerSelect == computerSelect):
+                    result = "Draw!";
+                    winner = "draw";
+                    break;
 
-            case ((computerSelect == "scissor" && playerSelect == "paper") || 
-                (computerSelect == "rock" && playerSelect == "scissor") || 
-                (computerSelect == "paper" && playerSelect == "rock") ):
-                result = "You Lose! " + computerSelect + " beats " + playerSelect + ".";
-                break;
-        }
+                case ((playerSelect == "scissor" && computerSelect == "paper") || 
+                    (playerSelect == "rock" && computerSelect == "scissor") || 
+                    (playerSelect == "paper" && computerSelect == "rock") ):
+                    result = "You Win! " + playerSelect + " beats " + computerSelect + "." ;
+                    winner = "player";
+                    break;
 
-    console.log (result);
-}
+                case ((computerSelect == "scissor" && playerSelect == "paper") || 
+                    (computerSelect == "rock" && playerSelect == "scissor") || 
+                    (computerSelect == "paper" && playerSelect == "rock") ):
+                    result = "You Lose! " + computerSelect + " beats " + playerSelect + ".";
+                    winner = "computer";
+                    break;
 
-gameRound ("scissor");
+                default:
+                    result = "Invalid Choice!"
+                    break;
+            }
+
+        alert (result);
+        console.log (playerSelect + "  " + computerSelect)
+        return winner;
+    }
+
+function oneGame (roundLimit)
+    {
+        let winner = "";
+            playerScore = 0, computerScore = 0;
+
+        while ((playerScore <= roundLimit-1) && (computerScore <= roundLimit-1))
+            {  
+                let playerSelect = playerSelection (), computerSelect = computerSelection ();
+
+                winner  = oneRound (playerSelect, computerSelect);
+
+                if (winner  == "player")
+                    {playerScore++;}
+
+                else if (winner  == "computer")
+                    {computerScore++;}
+
+                else if (winner  == "draw")
+                    {}
+                alert (playerScore + "   " + computerScore);
+            }
+    }
+
+oneGame (3)
+
 
